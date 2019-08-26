dd847e820dd53a2a9f621e6ef1f3bb666e9b4664
Matheus Quintino <matheus.q@live.com>
2019-08-25 22:47:02 -0300

    Quantity/Add to cart button

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,19 +1,102 @@
-import React from 'react';
-import { StatusBar, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StatusBar,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
+import ImageView from 'react-native-image-view';
+
 import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, ProductImage, ImageBg } from './styles';
+import {
+  Container,
+  ProductImage,
+  ImageBg,
+  InfoContainer,
+  NameAndPrice,
+  Name,
+  Price,
+  PriceContainer,
+  DescriptionContainer,
+  Description,
+  QtdAndButton,
+  QtdContainer,
+  MinusQtdButton,
+  QtdText,
+  AddToCart,
+  AddToCartText,
+} from './styles';
 
 export default function Details({ navigation }) {
   const data = navigation.getParam('data');
+  const images = [
+    {
+      source: { uri: data.pic },
+      title: data.name,
+      width: 800,
+      height: 600,
+    },
+  ];
+
+  const [visible, setVisible] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSetQuantity(bool) {
+    if (bool) {
+      return setQuantity(quantity + 1);
+    }
+
+    if (quantity > 1) {
+      return setQuantity(quantity - 1);
+    }
+    return null;
+  }
+
   return (
     <Container>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
       <ImageBg>
-        <ProductImage source={{ uri: data.pic }} />
+        <TouchableWithoutFeedback onPress={() => setVisible(true)}>
+          <ProductImage source={{ uri: data.pic }} />
+        </TouchableWithoutFeedback>
+        {visible && (
+          <ImageView
+            images={images}
+            imageIndex={0}
+            isVisible={visible}
+            onClose={() => setVisible(false)}
+          />
+        )}
       </ImageBg>
+
+      <InfoContainer>
+        <NameAndPrice>
+          <Name>{data.name}</Name>
+          <PriceContainer>
+            <Price>R${data.price}0</Price>
+          </PriceContainer>
+        </NameAndPrice>
+        <DescriptionContainer>
+          <Description>{data.description}</Description>
+        </DescriptionContainer>
+      </InfoContainer>
+
+      <QtdAndButton>
+        <QtdContainer>
+          <MinusQtdButton onPress={() => handleSetQuantity(false)}>
+            <Icon name="remove" size={15} color="#727C8E" />
+          </MinusQtdButton>
+          <QtdText> {quantity} </QtdText>
+          <MinusQtdButton onPress={() => handleSetQuantity(true)}>
+            <Icon name="add" size={15} color="#727C8E" />
+          </MinusQtdButton>
+        </QtdContainer>
+        <AddToCart>
+          <AddToCartText>Adicionar ao carrinho</AddToCartText>
+        </AddToCart>
+      </QtdAndButton>
     </Container>
   );
 }
@@ -24,7 +107,7 @@ Details.navigationOptions = ({ navigation }) => ({
 
   headerLeft: () => (
     <TouchableOpacity onPress={() => navigation.goBack()}>
-      <Icon name="chevron-left" size={20} color="#fff" />
+      <Icon name="chevron-left" size={30} color="#fff" />
     </TouchableOpacity>
   ),
 });
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RectButton, TouchableOpacity } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -15,4 +16,116 @@ export const ProductImage = styled.Image`
   border-bottom-right-radius: 25px;
 
   opacity: 0.5;
+`;
+
+export const InfoContainer = styled.View`
+  flex-direction: column;
+
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+export const NameAndPrice = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 30px;
+
+  padding-bottom: 5px;
+  border-bottom-width: 1px;
+  border-bottom-color: #ddd;
+`;
+
+export const Name = styled.Text`
+  font-weight: bold;
+  color: #333;
+  font-size: 22px;
+`;
+
+export const PriceContainer = styled.View`
+  border-radius: 4px;
+  background: #ff9f1c;
+  padding: 5px;
+`;
+
+export const Price = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+
+  color: #fff;
+`;
+
+export const DescriptionContainer = styled.View`
+  margin-top: 10px;
+  padding-bottom: 5px;
+`;
+
+export const Description = styled.Text`
+  color: #999;
+  font-size: 18px;
+`;
+
+export const QtdAndButton = styled.View`
+  margin-top: 30px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const QtdContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 45px;
+
+  padding-left: 15px;
+  padding-right: 15px;
+
+  background: #fff;
+  border-radius: 4px;
+`;
+
+export const MinusQtdButton = styled(TouchableOpacity)`
+  border-radius: 13px;
+  height: 26px;
+  width: 26px;
+
+  background: #b5bcc9;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DecreaseText = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const QtdText = styled.Text`
+  font-size: 20px;
+  margin: 20px;
+
+  text-align: center;
+  justify-content: center;
+`;
+
+export const AddToCart = styled(RectButton)`
+  background: #ff9f1c;
+  border-radius: 4px;
+
+  height: 45px;
+  padding-left: 10px;
+  padding-right: 10px;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AddToCartText = styled.Text`
+  color: #fff;
+  font-weight: bold;
 `;
